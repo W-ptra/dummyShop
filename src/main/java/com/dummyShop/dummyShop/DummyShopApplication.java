c10d2b5eba2a4f76242537622ada3e567ffeb266
W-ptr <wisnu.putra001@binus.ac.id>
2024-12-08 01:12:20 +0700

    initial commit

---
+++ src/main/java/com/dummyShop/dummyShop/DummyShopApplication.java
@@ -0,0 +1,13 @@
+package com.dummyShop.dummyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dummyShop/dummyShop/DummyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummyShop.dummyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
